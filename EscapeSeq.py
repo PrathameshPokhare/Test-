ee8aee7e65afdaa871eb008c3ec5f7a03653b612
PrathameshPokhare <pokhare.p@somaiya.edu>
2019-03-04 18:49:34 +0530

    Done

---
+++ EscapeSeq.py
@@ -0,0 +1,4 @@
+print("This is how \\\\ double backslash works")
+print("These are examples of backslash with forward-slash /\\/\\/\\/\\/\\")
+print("This is how tab\tworks")
+print("\\\" \\n \\\'")
